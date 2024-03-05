af7a59b46b029531d0c21d96200eaa7a300c4a8f
saravanaguhan19 <saravanaguhan19@gmail.com>
2024-03-05 17:59:36 +0530

    discounted book done

--- src/components/ui/Book.jsx
+++ src/components/ui/Book.jsx
@@ -15,9 +15,13 @@ function Book({ book }) {
         </a>
       </div>
       <div className="book__ratings">
-        {new Array(4).fill(0).map((_, index) => (
+        {new Array(Math.floor(book.rating)).fill(0).map((_, index) => (
           <FontAwesomeIcon icon="star" key={index} />
         ))}
+
+        {!Number.isInteger(book.rating) && (
+          <FontAwesomeIcon icon="star-half-alt" />
+        )}
       </div>
       <div className="book__price">
         {book.salePrice ? (
